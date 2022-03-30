d41b44ba0c501e617839d3914d2c26ddcc378c04
JiHongYuan <1206429202@qq.com>
2022-03-30 23:04:59 +0800

    feat: 插件开发，Main方法运行

---
+++ src/test/java/org/github/palace/bot/WordCloudPluginTest.java
@@ -0,0 +1,13 @@
+package org.github.palace.bot;
+
+import org.github.palace.bot.plugin.PluginManager;
+
+/**
+ * @author JHY
+ * @date 2022/3/30 23:02
+ */
+public class WordCloudPluginTest {
+    public static void main(String[] args) {
+        PluginManager plugin = new PluginManager();
+    }
+}
