1f130d75f9942308d32d49156bb614322c2dac77
JiHongYuan <1206429202@qq.com>
2022-04-08 16:03:36 +0800

    feat: 定时推送消息 增加jvm关闭生命周期

--- src/main/java/org/github/palace/bot/MainApplication.java
+++ src/main/java/org/github/palace/bot/MainApplication.java
@@ -3,8 +3,11 @@ package org.github.palace.bot;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.BotFactory;
 import org.github.palace.bot.builder.BotConfigurationBuilder;
-import org.github.palace.bot.constant.BaseConstant;
 import org.github.palace.bot.core.EventDispatcher;
+import org.github.palace.bot.core.cli.support.CommandManagerFactory;
+import org.github.palace.bot.core.cli.support.DefaultCommandManager;
+
+import static org.github.palace.bot.constant.BaseConstant.*;
 
 /**
  * @author JHY
@@ -12,11 +15,24 @@ import org.github.palace.bot.core.EventDispatcher;
  */
 public class MainApplication {
     public static void main(String[] args) {
-        Bot bot = BotFactory.INSTANCE.newBot(BaseConstant.QQ, BaseConstant.PASSWORD, BotConfigurationBuilder.builder());
+        Bot bot = BotFactory.INSTANCE.newBot(QQ, PASSWORD, BotConfigurationBuilder.builder());
         bot.login();
 
         EventDispatcher eventDispatcher = new EventDispatcher();
         eventDispatcher.start();
+
+        // 初始化命令管理器
+        CommandManagerFactory.setCommandManager(new DefaultCommandManager(COMMAND_PREFIX, bot));
+        // 启动命令推送
+        CommandManagerFactory.instance().startCommandPush();
+
+        // jvm关闭钩子函数
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // 关闭事件
+            eventDispatcher.stop();
+            // 关闭命令推送
+            CommandManagerFactory.instance().stopCommandPush();
+        }));
     }
 
 }
--- src/main/java/org/github/palace/bot/constant/BaseConstant.java
+++ src/main/java/org/github/palace/bot/constant/BaseConstant.java
@@ -13,10 +13,12 @@ public final class BaseConstant {
 
     public static final Long QQ;
     public static final String PASSWORD;
-
+    public static final String COMMAND_PREFIX;
     static {
         QQ = cast(YamlLoader.loadYamlNames("bot.base.qq", MainApplication.class.getClassLoader()), Long.class);
         PASSWORD = cast(YamlLoader.loadYamlNames("bot.base.password", MainApplication.class.getClassLoader()), String.class);
+        COMMAND_PREFIX = cast(YamlLoader.loadYamlNames("bot.base.command-prefix", MainApplication.class.getClassLoader()), String.class);
+
     }
 
     private BaseConstant() {
